46542930b0a6957ad10dc36a21207460e8b69912
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-03-21 22:25:40 +0700

    Add 01. variables

---
+++ 01. variables.py
@@ -0,0 +1,21 @@
+# variables
+
+x = 1 #int
+y = 2.5 #float
+name = 'yehezkel' #str (could be single or doublt quotes)
+is_cool = True #boolean (shold be capital initial letter)
+
+# multiple assignment
+x, y, name, is_cool = (1, 2.3, 'John', True)
+
+# basic math
+a = x + y
+
+# casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+print(x, y, name, is_cool)
+
+print(type(z), z)
